439f52745f34349cdbe7b012bae05799f6e824d0
pawelaksman <pablo_xmen@op.pl>
2024-05-18 15:59:29 +0200

    Removed functions

--- tests/product-page/steps.ts
+++ tests/product-page/steps.ts
@@ -10,30 +10,6 @@ export async function step(dataName: string, data: string, func: any) {
     })
 }
 
-export async function setSizeStep(productPage: ProductPage, product: Product) {
-    
-    await test.step('Click the "' + product.size,async () => {
-        
-        await productPage.setSize(product.size);
-    })
-}
-
-export async function setColorStep(productPage: ProductPage, product: Product) {
-    
-    await test.step('Click the "' + product.color + '"',async () => {
-        
-        await productPage.setColor(product.color);
-    })
-}
-
-export async function setQuantityeStep(productPage: ProductPage, quantity: string) {
-    
-    await test.step('Set quantity',async () => {
-        
-        await productPage.getQuantityField().setQuantity(quantity);
-    })
-}
-
 export async function clickAddToCartButtonStep(productPage: ProductPage) {
     
     await test.step('Click the "Add to cart" button',async () => {
