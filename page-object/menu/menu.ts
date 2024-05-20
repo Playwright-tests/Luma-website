a2cf0d26279ad336a4d57d562fe87a27bf60d955
pawelaksman <pablo_xmen@op.pl>
2024-05-21 00:25:53 +0200

    Renamed

---
+++ page-object/menu/menu.ts
@@ -0,0 +1,50 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/basePage";
+
+export class Menu extends BasePage {
+
+    private readonly womenListLocator: Locator;
+    private readonly menListLocator: Locator;
+    private readonly gearListLocator: Locator;
+    private readonly trainingListLocator: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.womenListLocator = page.getByText('TopsJacketsHoodies & SweatshirtsTeesBras & TanksBottomsPantsShorts');
+        this.menListLocator = page.getByText('TopsJacketsHoodies & SweatshirtsTeesTanksBottomsPantsShorts');
+        this.gearListLocator = page.getByText('BagsFitness EquipmentWatches');
+        this.trainingListLocator = page.locator('ul').filter({hasText: /^Video Download$/});
+    }
+
+    public async click(linkName: string): Promise<void> {
+
+        await this.getPage().getByRole('menuitem', {name: linkName}).click();
+    }
+
+    public async hover(linkName: string): Promise<void> {
+
+        await this.getPage().getByRole('menuitem', {name: linkName}).hover();
+    }
+
+    public getWomenListLocator(): Locator {
+
+        return this.womenListLocator;
+    }
+
+    public getMenListLocator(): Locator {
+
+        return this.menListLocator;
+    }
+
+    public getGearListLocator(): Locator {
+
+        return this.gearListLocator;
+    }
+
+    public getTrainingListLocator() {
+
+        return this.trainingListLocator;
+    }
+}
