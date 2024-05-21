7953327fe5ccb3dee08f184f60fc00a9d5bbcc12
pawelaksman <pablo_xmen@op.pl>
2024-05-21 23:07:58 +0200

    Updated

--- testdata-loaders/localTestdataLoader.ts
+++ testdata-loaders/localTestdataLoader.ts
@@ -2,6 +2,6 @@ export class LocalTestdataLoader {
 
     public static fetch(fileName: string): any {
 
-        return JSON.parse(JSON.stringify(require('../testdata' + fileName)));
+        return JSON.parse(JSON.stringify(require('../testdata/' + fileName)));
     }
 }
