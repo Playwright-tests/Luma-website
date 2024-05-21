7851c9ba3ab0f0a07203984775077e52a519b726
pawelaksman <pablo_xmen@op.pl>
2024-05-21 23:07:47 +0200

    Created methods

--- page-object/search-engine/searchEngine.ts
+++ page-object/search-engine/searchEngine.ts
@@ -1,15 +1,21 @@
 import { Locator, Page } from "@playwright/test";
 import { BasePage } from "../base/basePage";
+import { AutocompleteList } from "./autocompleteList";
 
 export class SearchEngine extends BasePage {
 
     private readonly searchField: Locator;
+    private readonly autocompleteListSelector = '#search_autocomplete';
+    private readonly autocompleteLocator: Locator;
+    private readonly autocompleteList: AutocompleteList;
 
     constructor(page: Page) {
 
         super(page);
 
+        this.autocompleteList = new AutocompleteList(page);
         this.searchField = page.locator('#search');
+        this.autocompleteLocator = page.locator('li[role="option"]');
     }
 
     public async enterPhrase(phrase: string): Promise<void> {
@@ -20,5 +26,20 @@ export class SearchEngine extends BasePage {
     public async getInput(): Promise<string | null> {
 
         return await this.searchField.inputValue();
+    }
+
+    public getAutocompleteListSelector() {
+
+        return this.autocompleteListSelector;
+    }
+
+    public getAutocompleteLocator(): Locator {
+
+        return this.autocompleteLocator;
+    }
+
+    public getAutocompleteList(): AutocompleteList {
+
+        return this.autocompleteList;
     }
 }
