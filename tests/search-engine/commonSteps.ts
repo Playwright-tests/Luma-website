b82c5763e39716a7b38f11aeee27dda79debf034
pawelaksman <pablo_xmen@op.pl>
2024-05-22 23:11:51 +0200

    Created steps

---
+++ tests/search-engine/commonSteps.ts
@@ -0,0 +1,16 @@
+import { test } from "@playwright/test";
+import { SearchEngine } from "../../page-object/search-engine/searchEngine";
+
+export async function steps(searchEngine: SearchEngine, phrase: string) {
+    
+    await test.step(`Enter the "${phrase}" phrase`,async () => {
+        
+        await searchEngine.enterPhrase(phrase);
+    })
+
+    await test.step('Press the ENTER key',async () => {
+        
+        await searchEngine.getPage().keyboard.press('Enter');
+    })
+
+}
