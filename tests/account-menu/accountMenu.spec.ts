318dc7768dcd183b2bfedf322356e914a1302d0a
pawelaksman <pablo_xmen@op.pl>
2024-05-23 23:37:57 +0200

    Created the class

---
+++ tests/account-menu/accountMenu.spec.ts
@@ -0,0 +1,21 @@
+import { PropertyNames } from "../../enums/enums";
+import { expect, test } from "../../fixtures/accountMenu";
+import { getLinks } from "../../testdata-providers/testDataProviders";
+
+const links = getLinks(PropertyNames.ACCOUNT_MENU);
+
+test.describe('The account menu',async () => {
+    
+    for(const link of links) {
+
+        test.only(`Clicking the "${link.link}" link`,async ({accountMenu}) => {
+            
+            await test.step(`Click the "${link.link}" link`,async () => {
+                
+                await accountMenu.clickLink(link.link);
+            })
+
+            await expect(accountMenu.getPage()).toHaveURL(link.url);
+        })
+    }
+})
