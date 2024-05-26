46c2c26aeb3aad944e297f841f7dd07e4abdd3c9
pawelaksman <pablo_xmen@op.pl>
2024-05-27 00:32:40 +0200

    Added the function

--- support/shoppingCartAction.ts
+++ support/shoppingCartAction.ts
@@ -2,16 +2,21 @@ import { Page } from "@playwright/test";
 import { Product } from "../models/models";
 import { ProductPage } from "../page-object/product-page/productPage";
 
-export async function shoppingCartAction(page: Page, products: Product[]) {
+export async function shoppingCartAction(page: Page, product: Product) {
     
     const productPage = new ProductPage(page);
 
+    await productPage.goto(product.url);
+    await productPage.setSize(product.size);
+    await productPage.setColor(product.color);
+    await productPage.quantityField.setQuantity(product.quantity);
+    await productPage.clickAddToCartButton();
+}
+
+export async function shoppingCartActions(page: Page, products: Product[]) {
+
     for(const product of products) {
 
-        await productPage.goto(product.url);
-        await productPage.setSize(product.size);
-        await productPage.setColor(product.color);
-        await productPage.quantityField.setQuantity(product.quantity);
-        await productPage.clickAddToCartButton();
+        await shoppingCartAction(page, product);
     }
 }
