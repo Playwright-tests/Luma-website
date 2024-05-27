c11718711924637f26f39dd54b24db9d094c9bf6
pawelaksman <pablo_xmen@op.pl>
2024-05-27 15:55:19 +0200

    Updated

--- tests/checkout/shippingMethods.spec.ts
+++ tests/checkout/shippingMethods.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "../../fixtures/checkout";
-import { radioButtonSteps } from "../../support/common-steps/checkout";
+import { radioButtonSteps } from "../../support/common-steps/checkoutSteps";
 import { getProducts } from "../../testdata-providers/testDataProviders";
 
 const product = getProducts()[0];
@@ -8,23 +8,23 @@ test.use({product: product});
 test.describe('Shipping methods radio buttons',async () => {
     
 
-    test.only('Clicking the "Fixed" radio button',async ({shippingMethods}) => {
+    test('Clicking the "Fixed" radio button',async ({checkoutpage}) => {
         
         await radioButtonSteps(async () => {
-            await shippingMethods.clickFixedRadioButton();
+            await checkoutpage.shippingMethods.clickFixedRadioButton();
         }, 'Fixed');
 
-        expect(shippingMethods.fixedRadioButtonLocator).toBeChecked();
-        expect(shippingMethods.tableRateRadioButtonLocator).not.toBeChecked();
+        expect(checkoutpage.shippingMethods.fixedRadioButtonLocator).toBeChecked();
+        expect(checkoutpage.shippingMethods.tableRateRadioButtonLocator).not.toBeChecked();
     })
 
-    test.only('Clicking the "Table Rate" radio button',async ({shippingMethods}) => {
+    test('Clicking the "Table Rate" radio button',async ({checkoutpage}) => {
         
         await radioButtonSteps(async () => {
-            await shippingMethods.clickTableRateRadioButton();
+            await checkoutpage.shippingMethods.clickTableRateRadioButton();
         }, 'Fixed');
 
-        expect(shippingMethods.fixedRadioButtonLocator).not.toBeChecked();
-        expect(shippingMethods.tableRateRadioButtonLocator).toBeChecked();
+        expect(checkoutpage.shippingMethods.fixedRadioButtonLocator).not.toBeChecked();
+        expect(checkoutpage.shippingMethods.tableRateRadioButtonLocator).toBeChecked();
     })
 })
