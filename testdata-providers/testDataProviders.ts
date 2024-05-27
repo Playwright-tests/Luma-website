76f75b62db4cb194d396ec1385a5b0fedb576864
pawelaksman <pablo_xmen@op.pl>
2024-05-27 15:54:34 +0200

    Created functions

--- testdata-providers/testDataProviders.ts
+++ testdata-providers/testDataProviders.ts
@@ -1,6 +1,6 @@
 import { credentials } from "../config/credentials";
-import { FileNames } from "../enums/enums";
-import { Credentials, LinkData, Product } from "../models/models";
+import { FileNames, PropertyNames } from "../enums/enums";
+import { Credentials, LinkData, Product, ShippingAddress, ShippingAddressIncorrect } from "../models/models";
 import { LocalTestdataLoader } from "../testdata-loaders/localTestdataLoader";
 
 export function getLinks(key: string) {
@@ -45,3 +45,19 @@ export function getIncorrectCredentials(key: string) {
     credentials = source[key];
     return credentials;
 }   
+
+export function getShippingAddress() {
+
+    let shippingAddress: ShippingAddress;
+    const source = LocalTestdataLoader.fetch(FileNames.SHIPPING_ADDRESS);
+    shippingAddress = source[PropertyNames.CORRECT];
+    return shippingAddress;
+}
+
+export function getIncorrectShippingAddressDatas() {
+
+    let incorrectDatas: ShippingAddressIncorrect;
+    const source = LocalTestdataLoader.fetch(FileNames.SHIPPING_ADDRESS_INCORRECT);
+    incorrectDatas = source[PropertyNames.INCORRECT];
+    return incorrectDatas;
+}
