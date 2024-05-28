180b711636934c7088b1ce1a0ddfc77b8e5645a6
pawelaksman <pablo_xmen@op.pl>
2024-05-28 23:33:12 +0200

    Updated

--- support/common-steps/accountInformationSteps.ts
+++ support/common-steps/accountInformationSteps.ts
@@ -1,10 +1,14 @@
 import { allure } from "allure-playwright";
 import { AccountInformation } from "../../page-object/account-information/accountInformation";
+import { takeScreenshot } from "../allure";
 
 export async function checkboxTestStep(accountInformation: AccountInformation, checkbox: string) {
     
     await allure.step(`Check the "${checkbox}" checkbox`,async () => {
         
+        checkbox === 'Change Email' ? await takeScreenshot('changeEmailField', accountInformation.changeEmailCheckboxLocator) :
+                                      await takeScreenshot('changePasswordField', accountInformation.changePasswordCheckboxLocator);
+
         checkbox === "Change Email" ? await accountInformation.clickChangeEmailCheckbox() : 
                                       await accountInformation.clickChangePasswordCheckbox();
     })
