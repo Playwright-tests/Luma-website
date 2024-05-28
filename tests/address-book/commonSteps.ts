d5722bb17436afc15a3490f913e2bd6fbe4018c0
pawelaksman <pablo_xmen@op.pl>
2024-05-28 15:19:25 +0200

    Removed

--- tests/address-book/commonSteps.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test";
-
-export async function inputVerificationStep(func: any, data: string) {
-    
-    await test.step(`Enter the "${data}"`,async () => {
-        
-        await func();
-    })
-}
