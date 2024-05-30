901215f2d7befa8ae7c5ccbc297b26b475770b2f
pawelaksman <pablo_xmen@op.pl>
2024-05-30 23:43:36 +0200

    Added the Allure reports

--- tests/checkout/shippingAddressForm.spec.ts
+++ tests/checkout/shippingAddressForm.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "../../fixtures/checkout";
 import { getProducts } from "../../testdata-providers/testDataProviders";
 import { inputVerificationStep } from "../../support/commonSteps";
 import { allure } from "allure-playwright";
+import { addParameters } from "../../support/allure";
 
 const product = getProducts()[0];
 test.use({product: product});
@@ -18,6 +19,10 @@ test.describe('The shipping address form input verification',async () => {
         
         const email = 'john@domain.com';
 
+        await allure.severity('normal');
+        await allure.feature('Filling the "Email Address" field');
+        await addParameters(new Map<string, string>([['Input', email]]));
+
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterEmail(email);
         }, 'Email Address', email);
@@ -28,6 +33,10 @@ test.describe('The shipping address form input verification',async () => {
     test('The "First Name" field input verification',async ({checkoutPage}) => {
         
         const firstName = 'First name';
+
+        await allure.severity('normal');
+        await allure.feature('Filling the "First Name" field');
+        await addParameters(new Map<string, string>([['Input', firstName]]));
 
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterFirstName(firstName);
@@ -40,6 +49,10 @@ test.describe('The shipping address form input verification',async () => {
         
         const lastName = 'Last name';
 
+        await allure.severity('normal');
+        await allure.feature('Filling the "Last Name" field');
+        await addParameters(new Map<string, string>([['Input', lastName]]));
+
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterLastName(lastName);
         }, 'Last Name', lastName);
@@ -50,6 +63,10 @@ test.describe('The shipping address form input verification',async () => {
     test('The "Company" field input verification',async ({checkoutPage}) => {
         
         const company = 'Company';
+
+        await allure.severity('normal');
+        await allure.feature('Filling the "Company" field');
+        await addParameters(new Map<string, string>([['Input', company]]));
 
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterCompany(company);
@@ -62,6 +79,10 @@ test.describe('The shipping address form input verification',async () => {
         
         const address = 'Address';
 
+        await allure.severity('normal');
+        await allure.feature('Filling the "Street Address" field');
+        await addParameters(new Map<string, string>([['Input', address]]))
+
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterAddress_1(address);
         }, 'Street Address line 1', address);
@@ -72,6 +93,10 @@ test.describe('The shipping address form input verification',async () => {
     test('The "Street Address" line 2 field input verification',async ({checkoutPage}) => {
         
         const address = 'Address';
+
+        await allure.severity('normal');
+        await allure.feature('Filling the "Street Address" line 2 field');
+        await addParameters(new Map<string, string>([['Input', address]]));
 
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterAddress_2(address);
@@ -84,6 +109,10 @@ test.describe('The shipping address form input verification',async () => {
         
         const address = 'Address';
 
+        await allure.severity('normal');
+        await allure.feature('Filling the "Street Address" line 3 field');
+        await addParameters(new Map<string, string>([['Input', address]]));
+
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterAddress_3(address);
         }, 'Street Address line 3', address);
@@ -94,6 +123,10 @@ test.describe('The shipping address form input verification',async () => {
     test('The "City" field input verification',async ({checkoutPage}) => {
         
         const city = 'City';
+
+        await allure.severity('normal');
+        await allure.feature('Filling the "City" field');
+        await addParameters(new Map<string, string>([['Input', city]]));
 
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterCity(city);
@@ -106,6 +139,10 @@ test.describe('The shipping address form input verification',async () => {
         
         const postcode = 'Postcode';
 
+        await allure.severity('normal');
+        await allure.feature('Filling the "Postcode" field');
+        await addParameters(new Map<string, string>([['Input', postcode]]));
+
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterPostcode(postcode);
         }, 'Postcode', postcode);
@@ -116,6 +153,10 @@ test.describe('The shipping address form input verification',async () => {
     test('The "Phone Number" field input verification',async ({checkoutPage}) => {
         
         const phone = '111222333';
+
+        await allure.severity('normal');
+        await allure.feature('Filling the "Phone Number" field');
+        await addParameters(new Map<string, string>([['Input', phone]]));
 
         await inputVerificationStep(async () => {
             await checkoutPage.shippingAddressForm.enterPhone(phone);
