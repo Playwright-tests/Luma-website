69e38567ae48255e026faec60b35e528ba91df55
pawelaksman <pablo_xmen@op.pl>
2024-06-05 23:11:02 +0200

    Added the Allure methods

--- tests/checkout/shippingMethods.spec.ts
+++ tests/checkout/shippingMethods.spec.ts
@@ -1,16 +1,24 @@
+import { allure } from "allure-playwright";
 import { test, expect } from "../../fixtures/checkout";
 import { radioButtonSteps } from "../../support/common-steps/checkoutSteps";
 import { getProducts } from "../../testdata-providers/testDataProviders";
+import { takeScreenshot } from "../../support/allure";
 
 const product = getProducts()[0];
 test.use({product: product});
 
 test.describe('Shipping methods radio buttons',async () => {
     
+    test.beforeEach(async () => {
+        
+        await allure.owner('Paweł Aksman');
+        await allure.tags('Radio buttons', 'Checkout');
+    })
 
     test('Clicking the "Fixed" radio button',async ({checkoutPage}) => {
         
         await radioButtonSteps(async () => {
+            await takeScreenshot('fixedRadioButton', checkoutPage.shippingMethods.fixedRadioButtonLocator);
             await checkoutPage.shippingMethods.clickFixedRadioButton();
         }, 'Fixed');
 
@@ -21,6 +29,7 @@ test.describe('Shipping methods radio buttons',async () => {
     test('Clicking the "Table Rate" radio button',async ({checkoutPage}) => {
         
         await radioButtonSteps(async () => {
+            await takeScreenshot('tableRateRadioButton', checkoutPage.shippingMethods.tableRateRadioButtonLocator);
             await checkoutPage.shippingMethods.clickTableRateRadioButton();
         }, 'Fixed');
 
