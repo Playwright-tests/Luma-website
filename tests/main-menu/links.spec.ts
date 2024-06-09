e744644c817a1a9ba97762d00c77917db2525fb7
aksi1984 <paksman3006@gmail.com>
2024-06-09 12:08:08 +0200

    Added the beforeEach

--- tests/main-menu/links.spec.ts
+++ tests/main-menu/links.spec.ts
@@ -1,19 +1,31 @@
 import { LinkTestdataProviders } from "../../testdata-providers/linkTestdataProviders";
-import { PropertyNames } from "../../enums/enums";
+import { PropertyNames, URLs } from "../../enums/enums";
 import { test, expect } from "../../fixtures/menu";
 import { allure } from "allure-playwright";
+import { takeScreenshot } from "../../support/allure";
 
 LinkTestdataProviders.load('links.json');
 const links = LinkTestdataProviders.get(PropertyNames.MAIN_MENU);
 
 test.describe('Main menu links',async () => {
     
+    test.beforeEach(async () => {
+        
+        await allure.owner('Paweł Aksman');
+        await allure.tags('Main menu', 'Links');
+        await allure.link(URLs.HOME_PAGE, 'Home page');
+    })
+
     for(const link of links) {
 
         test(`Clicking the "${link.link}" link`,async ({menu}) => {
-            
+  
+            await allure.severity('critical');
+            await allure.description(`Clicking the "${link.link}" link`);
+
             await allure.step(`Click the "${link.link}"`,async () => {
                 
+                await takeScreenshot(`link_${link.link}`, menu.getLocator(link.link));
                 await menu.click(link.link);
             })
 
