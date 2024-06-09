f38c73601389098659295b85f60464a1931f8f5d
aksi1984 <paksman3006@gmail.com>
2024-06-09 14:18:33 +0200

    Replaced the Allure features descriptions

--- tests/checkout/shippingAddressForm.spec.ts
+++ tests/checkout/shippingAddressForm.spec.ts
@@ -20,12 +20,12 @@ test.describe('The shipping address form input verification',async () => {
         const email = 'john@domain.com';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Email Address" field');
+        await allure.description('Verification of the "Email Address" field text input');
         await addParameters(new Map<string, string>([['Input', email]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('emailAddressField', checkoutPage.shippingAddressForm.emailFieldLocator);
             await checkoutPage.shippingAddressForm.enterEmail(email);
+            await takeScreenshot('emailAddressField', checkoutPage.shippingAddressForm.emailFieldLocator);
         }, 'Email Address', email);
 
         expect(await checkoutPage.shippingAddressForm.getEmailFieldInput()).toEqual(email);
@@ -36,12 +36,12 @@ test.describe('The shipping address form input verification',async () => {
         const firstName = 'First name';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "First Name" field');
+        await allure.description('Verification of the "First Name" field text input');
         await addParameters(new Map<string, string>([['Input', firstName]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('firstNameField', checkoutPage.shippingAddressForm.firstNameFieldLocator);
             await checkoutPage.shippingAddressForm.enterFirstName(firstName);
+            await takeScreenshot('firstNameField', checkoutPage.shippingAddressForm.firstNameFieldLocator);
         }, 'First Name', firstName);
 
         expect(await checkoutPage.shippingAddressForm.getFirstNameFieldInput()).toEqual(firstName);
@@ -52,12 +52,12 @@ test.describe('The shipping address form input verification',async () => {
         const lastName = 'Last name';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Last Name" field');
+        await allure.description('Verification of the "Last Name" field text input');
         await addParameters(new Map<string, string>([['Input', lastName]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('lastNameField', checkoutPage.shippingAddressForm.lastNameFieldLocator);
             await checkoutPage.shippingAddressForm.enterLastName(lastName);
+            await takeScreenshot('lastNameField', checkoutPage.shippingAddressForm.lastNameFieldLocator);
         }, 'Last Name', lastName);
 
         expect(await checkoutPage.shippingAddressForm.getLastNameFieldInput()).toEqual(lastName);
@@ -68,12 +68,12 @@ test.describe('The shipping address form input verification',async () => {
         const company = 'Company';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Company" field');
+        await allure.description('Verification of the "Company" field text input');
         await addParameters(new Map<string, string>([['Input', company]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('companyField', checkoutPage.shippingAddressForm.companyFieldLocator);
             await checkoutPage.shippingAddressForm.enterCompany(company);
+            await takeScreenshot('companyField', checkoutPage.shippingAddressForm.companyFieldLocator);
         }, 'Company', company);
 
         expect(await checkoutPage.shippingAddressForm.getCompanyFieldInput()).toEqual(company);
@@ -84,12 +84,12 @@ test.describe('The shipping address form input verification',async () => {
         const address = 'Address';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Street Address" field');
+        await allure.description('Verification of the "Street Address" line 1 field text input');
         await addParameters(new Map<string, string>([['Input', address]]))
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('addressField_1', checkoutPage.shippingAddressForm.addressFieldLocator_1);
             await checkoutPage.shippingAddressForm.enterAddress_1(address);
+            await takeScreenshot('addressField_1', checkoutPage.shippingAddressForm.addressFieldLocator_1);
         }, 'Street Address line 1', address);
 
         expect(await checkoutPage.shippingAddressForm.getAddressFieldInput_1()).toEqual(address);
@@ -100,12 +100,12 @@ test.describe('The shipping address form input verification',async () => {
         const address = 'Address';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Street Address" line 2 field');
+        await allure.description('Verification of the "Street Address" line 2 field text input');
         await addParameters(new Map<string, string>([['Input', address]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('addressField_2', checkoutPage.shippingAddressForm.addressFieldLocator_2);
             await checkoutPage.shippingAddressForm.enterAddress_2(address);
+            await takeScreenshot('addressField_2', checkoutPage.shippingAddressForm.addressFieldLocator_2);
         }, 'Street Address line 2', address);
 
         expect(await checkoutPage.shippingAddressForm.getAddressFieldInput_2()).toEqual(address);
@@ -116,12 +116,12 @@ test.describe('The shipping address form input verification',async () => {
         const address = 'Address';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Street Address" line 3 field');
+        await allure.description('Verification of the "Street Address" line 3 field text input');
         await addParameters(new Map<string, string>([['Input', address]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('addressField_3', checkoutPage.shippingAddressForm.addressFieldLocator_3);
             await checkoutPage.shippingAddressForm.enterAddress_3(address);
+            await takeScreenshot('addressField_3', checkoutPage.shippingAddressForm.addressFieldLocator_3);
         }, 'Street Address line 3', address);
 
         expect(await checkoutPage.shippingAddressForm.getAddressFieldInput_3()).toEqual(address);
@@ -132,12 +132,12 @@ test.describe('The shipping address form input verification',async () => {
         const city = 'City';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "City" field');
+        await allure.description('Verification of the "City" field text input');
         await addParameters(new Map<string, string>([['Input', city]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('firstNameField', checkoutPage.shippingAddressForm.cityFieldLocator);
             await checkoutPage.shippingAddressForm.enterCity(city);
+            await takeScreenshot('firstNameField', checkoutPage.shippingAddressForm.cityFieldLocator);
         }, 'City', city);
 
         expect(await checkoutPage.shippingAddressForm.getCityFieldInput()).toEqual(city);
@@ -148,12 +148,12 @@ test.describe('The shipping address form input verification',async () => {
         const postcode = 'Postcode';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Postcode" field');
+        await allure.description('Verification of the "Postcode" field text input');
         await addParameters(new Map<string, string>([['Input', postcode]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('postcodeField', checkoutPage.shippingAddressForm.postcodeFieldLocator);
             await checkoutPage.shippingAddressForm.enterPostcode(postcode);
+            await takeScreenshot('postcodeField', checkoutPage.shippingAddressForm.postcodeFieldLocator);
         }, 'Postcode', postcode);
 
         expect(await checkoutPage.shippingAddressForm.getPostcodeFieldInput()).toEqual(postcode);
@@ -164,12 +164,12 @@ test.describe('The shipping address form input verification',async () => {
         const phone = '111222333';
 
         await allure.severity('normal');
-        await allure.feature('Filling the "Phone Number" field');
+        await allure.description('Verification of the "Phone Number" field text input');
         await addParameters(new Map<string, string>([['Input', phone]]));
 
         await inputVerificationStep(async () => {
-            await takeScreenshot('phoneField', checkoutPage.shippingAddressForm.phoneFieldLocator);
             await checkoutPage.shippingAddressForm.enterPhone(phone);
+            await takeScreenshot('phoneField', checkoutPage.shippingAddressForm.phoneFieldLocator);
         }, 'Phone Number', phone);
 
         expect(await checkoutPage.shippingAddressForm.getPhoneFieldInput()).toEqual(phone);
